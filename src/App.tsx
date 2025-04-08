2f298257d9dca7edb7b1da03582d4a3ecb680acc
Asif <107271220+Asif2902@users.noreply.github.com>
2025-04-09 02:46:19 +0600

    update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -13,6 +12,8 @@ import DocFeesPriceImpact from "./pages/DocFeesPriceImpact";
 import DocSecurity from "./pages/DocSecurity";
 import DocTechnicalSpecifications from "./pages/DocTechnicalSpecifications";
 import DocTokenomics from "./pages/DocTokenomics";
+import DocRoadmap from "./pages/DocRoadmap";           // Add this line
+import DocDeepLinking from "./pages/DocDeepLinking";   // Add this line
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -35,6 +36,8 @@ const App = () => (
             <Route path="security" element={<DocSecurity />} />
             <Route path="technical-specifications" element={<DocTechnicalSpecifications />} />
             <Route path="tokenomics" element={<DocTokenomics />} />
+            <Route path="roadmap" element={<DocRoadmap />} />                 // Add this line
+            <Route path="deep-linking" element={<DocDeepLinking />} />        // Add this line
           </Route>
           
           {/* Catch-all route */}
